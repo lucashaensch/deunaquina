f0d58bab83bcb1284d947699c58d70eab2b9a5cd
Lucas <lucas.haensch@decora.do>
2016-04-26 19:21:27 -0300

    made more stuff

--- popup.js
+++ popup.js
@@ -1,11 +1,17 @@
 (function(){
-	var numbers = [];
-	var num;
-	for(var i = 0; i < 5; i++){
-		num = Math.floor(Math.random() * 79) + 1;
-		numbers.push(num);
-	};
-	numbers.sort(function(a, b){return a-b});
-	document.getElementsByTagName('body')[0].innerHTML = numbers.toString().replace(/,/g, '\t');
-})();
+	document.getElementsByClassName('choose')[0].onclick = function(ev){
+		document.getElementsByClassName('choose')[0].style.display = "none";
 
+		var numbers = [];
+		var numOfChoices = ev.target.innerHTML;
+		var num;
+		for(var i = 0; i < numOfChoices; i++){
+			num = Math.floor(Math.random() * 79) + 1;
+			numbers.push(num);
+		};
+		numbers = numbers.sort(function(a, b){return a-b}).toString().replace(/,/g, '\t');
+		// document.getElementsByTagName('body')[0].innerHTML = numbers;
+
+		document.getElementsByClassName('raffled')[0].innerHTML = numbers;
+	};
+})();
