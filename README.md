3c47a9f6bff65bdb06bbef932f37543bb327d0c7
lucas haensch <lucashaensch@gmail.com>
2016-05-17 00:47:33 -0300

    changed everything

---
+++ README.md
@@ -0,0 +1,14 @@
+Mega-sena
+	6 a 15 numeros jogados de 1 a 60
+
+Lotofacil
+	15 a 18 numeros jogados de 1 a 25
+
+Quina
+	5 a 7 numeros jogados de 1 a 80
+
+Lotomania
+	0 ou 15 a 20 numeros jogados de 1 a 99
+
+Dupla Sena
+	6 a 15 numeros jogados de 1 a 50
--- popup.js
+++ popup.js
@@ -1,17 +1,30 @@
 (function(){
-	document.getElementsByClassName('choose')[0].onclick = function(ev){
-		document.getElementsByClassName('choose')[0].style.display = "none";
+	// document.getElementsByClassName('choose')[0].onclick = function(ev){
+	// 	document.getElementsByClassName('choose')[0].style.display = "none";
 
-		var numbers = [];
-		var numOfChoices = ev.target.innerHTML;
-		var num;
-		for(var i = 0; i < numOfChoices; i++){
-			num = Math.floor(Math.random() * 79) + 1;
-			numbers.push(num);
-		};
-		numbers = numbers.sort(function(a, b){return a-b}).toString().replace(/,/g, '\t');
-		// document.getElementsByTagName('body')[0].innerHTML = numbers;
+	// 	var numbers = [];
+	// 	var numOfChoices = ev.target.innerHTML;
+	// 	var num;
+	// 	for(var i = 0; i < numOfChoices; i++){
+	// 		num = Math.floor(Math.random() * 79) + 1;
+	// 		numbers.push(num);
+	// 	};
+	// 	// numbers = numbers.sort(function(a, b){return a-b}).toString().replace(/,/g, '\t');
+	// 	// // document.getElementsByTagName('body')[0].innerHTML = numbers;
 
-		document.getElementsByClassName('raffled')[0].innerHTML = numbers;
-	};
+	// 	var raffledLis = '';
+
+	// 	for (var i = 0; i < numbers.length; i++) {
+	// 		raffledLis += "<li>";
+	// 		raffledLis += numbers[i];
+	// 		raffledLis += "</li>";
+	// 	};
+
+	// 	var raffledElement = document.getElementsByClassName('raffled')[0];
+	// 	var numbersElement = document.getElementsByClassName('numbers')[0];
+
+	// 	numbersElement.innerHTML = raffledLis;
+	// 	raffledElement.className += ' active';
+	// };
+	alert($('body'));
 })();
